0ece1e894652a8d6aa54652bdc3f785f2ced69b0
Riccardo Rebottini <ricky@rebottini.it>
2020-11-09 21:20:12 +0100

    funct compnent -> class component + tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
+import GaugesController from './gauges-controller';
+
 interface Props {
   text: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+class ExampleComponent extends React.Component<Props, {}> {
+  constructor(props: Props) {
+    super(props);
+
+	}
+	
+	componentDidMount() {
+		let tempCtrl = new GaugesController();
+		tempCtrl.getRealTime();
+	}
+
+  render() {
+    return (
+      <div>
+        <div className={styles.test}>Example Component: {this.props.text}</div>
+      </div>
+    )
+  }
+
 }
+
+export { ExampleComponent };
