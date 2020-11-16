dc66576eaa458f02047c26a35aaab8854a498a44
Riccardo Rebottini <ricky@rebottini.it>
2020-11-16 18:56:39 +0100

    testing temp gauge

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,30 @@ import * as React from 'react'
 import styles from './styles.module.css'
 
 import GaugesController from './gauges-controller';
+import TempGauge from './gauges/temp';
+import GaugeSize from './style/gaugeSize';
 
 interface Props {
   text: string
 }
 
 class ExampleComponent extends React.Component<Props, {}> {
+  controller: any;
+
   constructor(props: Props) {
     super(props);
-
+    this.controller = new GaugesController();
 	}
 	
 	componentDidMount() {
-		let tempCtrl = new GaugesController();
-		tempCtrl.getRealTime();
+		this.controller.getRealTime();
 	}
 
   render() {
     return (
       <div>
         <div className={styles.test}>Example Component: {this.props.text}</div>
+        <TempGauge controller={this.controller} size={GaugeSize.Sml} />
       </div>
     )
   }
