1e32fb56a7d127cc1e3ea789d568d6d3b538085a
Riccardo Rebottini <ricky@rebottini.it>
2020-11-17 11:09:45 +0100

    testing new gauges

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import styles from './styles.module.css'
 import GaugesController from './gauges-controller';
 import TempGauge from './gauges/temp';
 import GaugeSize from './style/gaugeSize';
+import StatusScrollerGauge from './gauges/status-scroller';
+import LedGauge from './gauges/led';
+import StatusTimerGauge from './gauges/status-timer';
 
 interface Props {
   text: string
@@ -25,7 +28,17 @@ class ExampleComponent extends React.Component<Props, {}> {
     return (
       <div>
         <div className={styles.test}>Example Component: {this.props.text}</div>
-        <TempGauge controller={this.controller} size={GaugeSize.Sml} />
+        <div style={{ marginLeft: 'auto', marginRight: 'auto', textAlign: 'center' }}>
+          <div>
+            <LedGauge controller={this.controller} />
+            <StatusScrollerGauge controller={this.controller} width={550} /> &nbsp;
+            <StatusTimerGauge controller={this.controller} width={70} />
+          </div>
+          <div>
+            <TempGauge controller={this.controller} size={GaugeSize.Sml} />
+          </div>
+        </div>
+        
       </div>
     )
   }
