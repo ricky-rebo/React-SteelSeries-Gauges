c0b70b03cd7ab0a5dd55fdb6c7a7b9b4a13e8fa7
Riccardo Rebottini <ricky@rebottini.it>
2021-03-17 20:25:33 +0100

    refactored SolarGauge

--- src/gauges/solar.tsx
+++ src/gauges/solar.tsx
@@ -2,58 +2,48 @@ import React, { Component } from 'react';
 // @ts-ignore
 import { Radial, LedColor, Section } from "steelseries";
 import styles from '../style/common.css';
-import { ERR_VAL } from '../controller/utils';
 import { gaugeShadow, nextHighest } from './utils.js';
 import { RtData } from '../controller/types.js';
-import { CommonProps } from './types';
+import { CommonProps, RGBAColor } from './types';
+import { getCommonParams, SHADOW_COLOR, SHOW_GAUGE_SHADOW, SHOW_SUNSHINE_LED, SolarScaleDef, SUNSHINE_PCT_TRESHOLD, SUNSHINE_TRESHOLD } from './defaults';
 
 
 class SolarGauge extends Component<CommonProps, State> {
 	static NAME = "SOLAR_GAUGE";
 
 	canvasRef: React.RefObject<HTMLCanvasElement>;
-	gauge: any;
-	params: any;
+	gauge: Radial;
+
+	config: Config;
+
 	style: React.CSSProperties;
 
 	constructor(props: CommonProps) {
 		super(props);
 
 		this.canvasRef = React.createRef();
+
+		this.config = {
+			scaleDefMax: SolarScaleDef.Max_Solar,
+			showSunshineLed: SHOW_SUNSHINE_LED,
+			showGaugeShadow: SHOW_GAUGE_SHADOW,
+			shadowColor: SHADOW_COLOR,
+			sunshineThreshold: SUNSHINE_TRESHOLD,
+			sunshineThresholdPct: SUNSHINE_PCT_TRESHOLD
+		}
 	
 		this.state = {
-			value:  0.0001,
-			maxValue: this.props.controller.gaugeConfig.solarGaugeScaleMax,
+			value: 0.0001,
+			maxValue: this.config.scaleDefMax,
 			maxToday: 0,
 			area: [],
 			ledState : false,
 
-			//popUpTxt: '',
-			//popUpGraph: '',
+			//popUpTxt: ''
 		};
 
-		this.params = {
-			...this.props.controller.commonParams,
-			size: Math.ceil(this.props.size * this.props.controller.gaugeConfig.gaugeScaling),
-			section: [
-				Section(0, 600, 'rgba(40,149,0,0.3)'),
-				Section(600, 800, 'rgba(248,89,0,0.3)'),
-				Section(800, 1000, 'rgba(216,0,29,0.3)'),
-				Section(1000, 1800, 'rgba(107,73,200,0.3)')
-			],
-			maxValue: this.state.maxValue,
-			titleString: this.props.controller.lang.solar_title,
-			niceScale: false,
-			unitString: 'W/m\u00B2',
-			thresholdVisible: false,
-			lcdDecimals: 0,
-			userLedVisible : this.props.controller.gaugeConfig.showSunshineLed,
-			userLedColor : LedColor.YELLOW_LED,
-			maxMeasuredValueVisible: true,
-		};
-
-		this.style = this.props.controller.gaugeConfig.showGaugeShadow
-			? gaugeShadow(this.params.size, this.props.controller.gaugeConfig.shadowColour)
+		this.style = this.config.showGaugeShadow
+			? gaugeShadow(this.props.size, this.config.shadowColor)
 			: {};
 
 		this.update = this.update.bind(this);
@@ -63,7 +53,27 @@ class SolarGauge extends Component<CommonProps, State> {
 
 	componentDidMount() {
 		if(this.canvasRef.current) {
-			this.gauge = new Radial(this.canvasRef.current, this.params);
+			this.gauge = new Radial(this.canvasRef.current, {
+				...getCommonParams(),
+	
+				size: this.props.size,
+				section: [
+					Section(0, 600, 'rgba(40,149,0,0.3)'),
+					Section(600, 800, 'rgba(248,89,0,0.3)'),
+					Section(800, 1000, 'rgba(216,0,29,0.3)'),
+					Section(1000, 1800, 'rgba(107,73,200,0.3)')
+				],
+				area: this.state.area,
+				maxValue: this.state.maxValue,
+				titleString: this.props.controller.lang.solar_title,
+				niceScale: false,
+				unitString: 'W/m\u00B2',
+				thresholdVisible: false,
+				lcdDecimals: 0,
+				userLedVisible : this.config.showSunshineLed,
+				userLedColor : LedColor.YELLOW_LED,
+				maxMeasuredValueVisible: true,
+			});
 			this.gauge.setValue(this.state.value);
 		}
 	}
@@ -75,31 +85,29 @@ class SolarGauge extends Component<CommonProps, State> {
 		newState.maxToday = SolarTM;
 		newState.currMaxValue = CurrentSolarMax;
 
-		newState.maxValue = Math.max(newState.value, newState.currMaxValue, newState.maxToday, this.props.controller.gaugeConfig.solarGaugeScaleMax);
-		newState.maxValue = nextHighest(newState.maxValue, 100);
+		newState.maxValue = nextHighest(
+			Math.max(newState.value, newState.currMaxValue, newState.maxToday, this.config.scaleDefMax), 
+			100
+		);
 
+		newState.area=[
+			// Sunshine threshold
+			Section(
+				Math.max(newState.currMaxValue * this.config.sunshineThresholdPct / 100, this.config.sunshineThreshold),
+				newState.currMaxValue,
+				'rgba(255,255,50,0.4)'
+			),
+			// Over max threshold
+			Section(
+				newState.currMaxValue,
+				Math.min(newState.currMaxValue + newState.maxValue * 0.15, newState.maxValue),
+				'rgba(220,0,0,0.5)'
+			)
+		]
 
-		let { sunshineThresholdPct, sunshineThreshold } = this.props.controller.gaugeConfig;
-		if(CurrentSolarMax !== ERR_VAL){
-			newState.area=[
-				// Sunshine threshold
-				Section(
-					Math.max(newState.currMaxValue * sunshineThresholdPct / 100, sunshineThreshold),
-					newState.currMaxValue,
-					'rgba(255,255,50,0.4)'
-				),
-				// Over max threshold
-				Section(
-					newState.currMaxValue,
-					Math.min(newState.currMaxValue + newState.maxValue * 0.15,newState.maxValue),
-					'rgba(220,0,0,0.5)'
-				)
-			]
-		}
-
-		if(this.params.userLedVisible) {
+		if(this.config.showSunshineLed) {
 			let percent = newState.currMaxValue === 0 ? '--' : Math.round(newState.value / newState.currMaxValue * 100);
-			newState.ledState = (percent !== '--') && (percent >= sunshineThresholdPct) && (newState.value >= sunshineThreshold);
+			newState.ledState = (percent !== '--') && (percent >= this.config.sunshineThresholdPct) && (newState.value >= this.config.sunshineThreshold);
 		}
 
 		this.setState(newState);
@@ -107,6 +115,7 @@ class SolarGauge extends Component<CommonProps, State> {
 
 	componentDidUpdate(_prevProps: CommonProps, prevState: State) {
 		if(prevState.maxValue !== this.state.maxValue) {
+			this.gauge.setValue(0);
 			this.gauge.setMaxValue(this.state.maxValue)
 		}
 		
@@ -118,7 +127,6 @@ class SolarGauge extends Component<CommonProps, State> {
 			this.gauge.setUserLedOnOff(this.state.ledState);
 		}
 		
-		
 		this.gauge.setValueAnimated(this.state.value);
 		this.gauge.setMaxMeasuredValue(this.state.maxToday);
 	}
@@ -128,8 +136,8 @@ class SolarGauge extends Component<CommonProps, State> {
 			<div className={styles.gauge}>
 				<canvas 
 					ref={this.canvasRef}
-					width={this.params.size}
-					height={this.params.size}
+					width={this.props.size}
+					height={this.props.size}
 					style={this.style}
 				></canvas>
 			</div>
@@ -137,11 +145,21 @@ class SolarGauge extends Component<CommonProps, State> {
 	}
 }
 
+
+interface Config {
+	scaleDefMax: number,
+	showSunshineLed: boolean,
+	showGaugeShadow: boolean,
+	shadowColor: RGBAColor,
+	sunshineThreshold: number,
+	sunshineThresholdPct: number
+}
+
 interface State {
 	value: number,
 	maxValue: number,
 	maxToday: number,
-	area: [],
+	area: Section[],
 
 	ledState: boolean,
 }
