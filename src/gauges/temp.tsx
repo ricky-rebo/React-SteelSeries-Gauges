fe1ec122ccf5065a438a89e0a4bb5e3aaaa90372
Riccardo Rebottini <ricky@rebottini.it>
2021-03-18 14:40:13 +0100

    refactored Temp gauge

--- src/gauges/temp.tsx
+++ src/gauges/temp.tsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 // @ts-ignore
-import { Radial, TrendState, Section } from "steelseries";
+import { Radial, TrendState, Section, Trendstate } from "steelseries";
 import styles from '../style/common.css';
 import Cookies from 'universal-cookie/es6';
-import { InOutType, CommonProps } from './types';
-import { createTempSections, gaugeShadow, getMinTemp, getMaxTemp, tempTrend } from './utils.js';
-import { RtData, TempUnit } from '../controller/types.js';
+import { InOutType, CommonProps, RGBAColor } from './types';
+import { gaugeShadow, getMinTemp, getMaxTemp } from './utils.js';
+import { Lang, RtData, TempUnit } from '../controller/types.js';
+import { getCommonParams, MIN_MAX_AREA_COLOR, SHADOW_COLOR, SHOW_GAUGE_SHADOW, SHOW_TEMP_INDOOR, SHOW_TEMP_TREND, TempScaleDef } from './defaults';
 
 const COOKIE_NAME = 'temp-display';
-
 
 class TempGauge extends Component<CommonProps, State> {
 	static NAME = "TEMP_GAUGE";
 
 	canvasRef: React.RefObject<HTMLCanvasElement>;
-	gauge: any;
-	params: any;
+	gauge: Radial;
+
+	config: Config;
+
 	style: React.CSSProperties;
 	cookies: Cookies;
 
@@ -24,29 +26,41 @@ class TempGauge extends Component<CommonProps, State> {
 
 		this.canvasRef = React.createRef();
 
+		this.config = {
+			scaleDef: TempScaleDef,
+			showIndoor: SHOW_TEMP_INDOOR,
+			showTrend: SHOW_TEMP_TREND,
+			minMaxAreaColor: MIN_MAX_AREA_COLOR,
+
+
+			showGaugeShadow: SHOW_GAUGE_SHADOW,
+			shadowColor: SHADOW_COLOR
+		}
+
 		let tempType: InOutType = "out";
-		if(props.controller.config.useCookies && props.controller.gaugeConfig.showIndoorTempHum) {
+		if(props.controller.config.useCookies && this.config.showIndoor) {
 			this.cookies = new Cookies();
 
 			let sel = this.cookies.get(COOKIE_NAME);
 			if(sel) tempType = sel;
 			else {
-				//TODO set expire date
-				this.cookies.set(COOKIE_NAME, tempType);
+				let expireDate = new Date();
+				expireDate.setFullYear(expireDate.getFullYear() + 1);
+				this.cookies.set(COOKIE_NAME, tempType, { path: "/", expires: expireDate });
 			}
 		}
 		
 		let { temp } = props.controller.getDisplayUnits();
 		this.state = {
 			value: (temp === "°C")
-				? this.props.controller.gaugeConfig.tempScaleDefMinC + 0.0001
-				: this.props.controller.gaugeConfig.tempScaleDefMinF + 0.0001,
+				? this.config.scaleDef.Min_C + 0.0001
+				: this.config.scaleDef.Min_F + 0.0001,
 			minValue: (temp === "°C")
-				? this.props.controller.gaugeConfig.tempScaleDefMinC
-				: this.props.controller.gaugeConfig.tempScaleDefMinF,
+				? this.config.scaleDef.Min_C
+				: this.config.scaleDef.Min_F,
 			maxValue: (temp === "°C")
-				? this.props.controller.gaugeConfig.tempScaleDefMaxC
-				: this.props.controller.gaugeConfig.tempScaleDefMaxF,
+				? this.config.scaleDef.Max_C
+				: this.config.scaleDef.Max_F,
 			trend: TrendState.OFF,
 			areas: [],
 
@@ -55,31 +69,14 @@ class TempGauge extends Component<CommonProps, State> {
 				: this.props.controller.lang.temp_title_in,
 			displayUnit: temp,
 			sections: createTempSections(temp === "°C"),
-			maxMinVisible: false,
 
-			//popUpTxt: '',
-			//graph: '',
+			//popUpTxt: ''
 			
 			selected: tempType
 		}
 
-		this.params = {
-			...this.props.controller.commonParams,
-			size: Math.ceil(this.props.size * this.props.controller.gaugeConfig.gaugeScaling),
-			section: this.state.sections,
-			area: [],
-			minValue: this.state.minValue,
-			maxValue: this.state.maxValue,
-			thresholdVisible: false,
-			minMeasuredValueVisible: this.state.maxMinVisible,
-			maxMeasuredValueVisible: this.state.maxMinVisible,
-			titleString: this.state.title,
-			unitString: this.state.displayUnit,
-			trendVisible: this.props.controller.gaugeConfig.showTempTrend
-		};
-
-		this.style = this.props.controller.gaugeConfig.showGaugeShadow
-			? gaugeShadow(this.params.size, this.props.controller.gaugeConfig.shadowColour)
+		this.style = this.config.showGaugeShadow
+			? gaugeShadow(this.props.size, this.config.shadowColor)
 			: {};
 
 		this.update = this.update.bind(this);
@@ -89,7 +86,21 @@ class TempGauge extends Component<CommonProps, State> {
 
 	componentDidMount() {
 		if(this.canvasRef.current) {
-			this.gauge = new Radial(this.canvasRef.current, this.params);
+			this.gauge = new Radial(this.canvasRef.current, {
+				...getCommonParams(),
+				
+				size: this.props.size,
+				section: this.state.sections,
+				area: [],
+				minValue: this.state.minValue,
+				maxValue: this.state.maxValue,
+				thresholdVisible: false,
+				minMeasuredValueVisible: false,
+				maxMeasuredValueVisible: false,
+				titleString: this.state.title,
+				unitString: this.state.displayUnit,
+				trendVisible: this.config.showTrend
+			});
 			this.gauge.setValue(this.state.value);
 		}
 	}
@@ -123,77 +134,59 @@ class TempGauge extends Component<CommonProps, State> {
 			newState.selected = this.state.selected;
 		}
 
-		newState.minValue = data.tempunit === "°C"
-			? this.props.controller.gaugeConfig.tempScaleDefMinC
-			: this.props.controller.gaugeConfig.tempScaleDefMinF;
-		newState.maxValue = data.tempunit === "°C"
-			? this.props.controller.gaugeConfig.tempScaleDefMaxC
-			: this.props.controller.gaugeConfig.tempScaleDefMaxF;
+		newState.value = (newState.selected === "out")
+			? data.temp
+			: data.intemp;
 
-		let lowScale: number, highScale: number;
-		if(newState.selected === "out") {
-			newState.value = data.temp;
-			
-			lowScale = newState.minValue, data;
-			highScale = newState.maxValue, data;
-
-			//loc = this.props.controller.lang.temp_out_info;
-			
-			if(this.params.trendVisible) {
-				let trendVal = data.temptrend;
-				newState.trend = tempTrend(trendVal, data.tempunit, false);
-			}
-
-			let low = data.tempTL;
-			let high = data.tempTH;
-			newState.areas = [Section(low, high, this.props.controller.gaugeConfig.minMaxArea)];
-		}
-		else {
-			//Indoor selected 
-			newState.value = data.intemp;
-
-			if (data.intempTL && data.intempTH) { // Indoor - and Max/Min values supplied
-				lowScale = getMinTemp(newState.minValue, data);
-				highScale = getMaxTemp(newState.maxValue, data);
-
-				let low = data.intempTL;
-				let high = data.intempTH;
-				newState.areas = [Section(low, high, this.props.controller.gaugeConfig.minMaxArea)];
-			}
-			else { // Indoor - no Max/Min values supplied
-				lowScale = highScale = newState.value;
-				newState.areas = [];
-			}
-
-			if (this.params.trendVisible) {
+		if(this.config.showTrend) {
+			if(newState.selected === "out")
+				newState.trend = tempTrend(data.temptrend, data.tempunit);
+			else
 				newState.trend = TrendState.OFF;
-			}
 		}
-		
+
+		let low = (newState.selected === "out")
+			? data.tempTL
+			: data.intempTL;
+		let high = (newState.selected === "out")
+			? data.tempTH
+			: data.intempTH;
+		newState.areas = [Section(low, high, this.config.minMaxAreaColor)];
+
+
 		// auto scale the ranges
+		let minValue = (data.tempunit === "°C")
+			? this.config.scaleDef.Min_C
+			: this.config.scaleDef.Min_F;
+		let maxValue = (data.tempunit === "°C")
+			? this.config.scaleDef.Max_C
+			: this.config.scaleDef.Max_F;
+		let lowScale = getMinTemp(minValue, data);
+		let highScale = getMaxTemp(maxValue, data);
 		let scaleStep = data.tempunit === "°C" ? 10 : 20;
 		while (lowScale < newState.minValue) {
-			newState.minValue -= scaleStep;
+			minValue -= scaleStep;
 			if (highScale <= newState.maxValue - scaleStep) {
-				newState.maxValue -= scaleStep;
+				maxValue -= scaleStep;
 			}
 		}
 		
 		while (highScale > newState.maxValue) {
-			newState.maxValue += scaleStep;
+			maxValue += scaleStep;
 			if (newState.minValue >= newState.minValue + scaleStep) {
-				newState.minValue += scaleStep;
+				minValue += scaleStep;
 			}
 		}
 
+		if(minValue < this.state.minValue) newState.minValue = minValue;
+		if(maxValue > this.state.maxValue) newState.maxValue = maxValue;
+		
 		this.setState(newState);
 	}
 
 	componentDidUpdate(_prevProps: CommonProps, prevState: State) {
 		if(prevState.selected !== this.state.selected) {
 			this.gauge.setTitleString(this.state.title);
-			this.gauge.setMaxMeasuredValueVisible(this.state.maxMinVisible);
-			this.gauge.setMinMeasuredValueVisible(this.state.maxMinVisible);
 		}
 
 		if(this.state.displayUnit !== prevState.displayUnit) {
@@ -203,11 +196,11 @@ class TempGauge extends Component<CommonProps, State> {
 
 		if (this.state.minValue !== this.gauge.getMinValue() || this.state.maxValue !== this.gauge.getMaxValue()) {
 			this.gauge.setMinValue(this.state.minValue);
-			this.gauge.setMaxValue(this.state.maxValue);
 			this.gauge.setValue(this.state.minValue);
+			this.gauge.setMaxValue(this.state.maxValue);
 		}
 
-		if (this.params.trendVisible) {
+		if (this.config.showTrend) {
 			this.gauge.setTrend(this.state.trend);
 		}
 
@@ -216,55 +209,61 @@ class TempGauge extends Component<CommonProps, State> {
 	}
 
 	render() {
-		return <div className={styles.gauge}>
-			<div id="tip_0">
+		return (
+			<div className={styles.gauge}>
 				<canvas 
 					ref={this.canvasRef}
-					width={this.params.size}
-					height={this.params.size}
+					width={this.props.size}
+					height={this.props.size}
 					style={this.style}
 				></canvas>
+				<div>
+					<button onClick={() => this.setInOutTemp('out')}>Out</button>
+					<button onClick={() => this.setInOutTemp('in')}>In</button>
+				</div>
+				<div>
+					<button onClick={() => this.props.controller.changeUnits({ temp: "°C"})}> °C </button>
+					<button onClick={() => this.props.controller.changeUnits({ temp: "°F"})}> °F </button>
+				</div>
 			</div>
-			<div>
-				<button onClick={() => this.setInOutTemp('out')}>Out</button>
-				<button onClick={() => this.setInOutTemp('in')}>In</button>
-			</div>
-			<div>
-				<button onClick={() => this.props.controller.changeUnits({ temp: "°C"})}> °C </button>
-				<button onClick={() => this.props.controller.changeUnits({ temp: "°F"})}> °F </button>
-			</div>
-		</div>
+		)
 	}
+}
+
+
+interface Config {
+	scaleDef: typeof TempScaleDef,
+	showIndoor: boolean,
+	showTrend: boolean,
+	minMaxAreaColor: RGBAColor,
+
+
+	showGaugeShadow: boolean,
+	shadowColor: RGBAColor
 }
 
 interface State {
 	data?: LocalDataDef,
 
 	displayUnit: TempUnit,
-	maxMinVisible: boolean,
 	selected: InOutType,
 
 	value: number,
 	minValue: number,
 	maxValue: number,
-	trend: any,
+	trend: TrendState,
 	title: string,
-	areas: any[],
-	sections: any
+	areas: Section[],
+	sections: Section[]
 
-	//popUpTxt: string,
-	//graph: string
+	//popUpTxt: string
 }
 
-export interface LocalDataDef {
-	temp: number, tempunit: TempUnit, temptrend: number,
-	tempTL: number, dewpointTL: number, apptempTL: number, wchillTL: number,
-	tempTH: number, apptempTH: number, heatindexTH: number, humidex: number,
-	intemp: number, intempTL?: number, intempTH?: number
-}
+export type LocalDataDef = Pick<RtData, "temp"|"tempunit"|"temptrend"|"tempTL"|"dewpointTL"|"apptempTL"|"wchillTL"|"tempTH"|"apptempTH"|"heatindexTH"|"humidex"|"intemp"|"intempTL"|"intempTH">
 
-function mapLocalData(data: RtData) {
-	let locData: LocalDataDef = {
+
+function mapLocalData(data: RtData): LocalDataDef {
+	return {
 		temp				: data.temp,
 		tempunit		: data.tempunit,
 		temptrend		: data.temptrend,
@@ -280,7 +279,77 @@ function mapLocalData(data: RtData) {
 		intempTL		: data.intempTL,
 		intempTH		: data.intempTH
 	}
-	return locData;
+}
+
+/**
+ * //TODO move in Temp Gauge
+ * Creates an array of gauge sections appropriate for Celsius or Fahrenheit scales
+ * @param useCelsius True id you're using Celsius, False otherwise
+ */
+function createTempSections (useCelsius: boolean) {
+	if (useCelsius) {
+		return [
+			Section(-100, -35, 'rgba(195, 92, 211, 0.4)'),
+			Section(-35, -30, 'rgba(139, 74, 197, 0.4)'),
+			Section(-30, -25, 'rgba(98, 65, 188, 0.4)'),
+			Section(-25, -20, 'rgba(62, 66, 185, 0.4)'),
+			Section(-20, -15, 'rgba(42, 84, 194, 0.4)'),
+			Section(-15, -10, 'rgba(25, 112, 210, 0.4)'),
+			Section(-10, -5, 'rgba(9, 150, 224, 0.4)'),
+			Section(-5, 0, 'rgba(2, 170, 209, 0.4)'),
+			Section(0, 5, 'rgba(0, 162, 145, 0.4)'),
+			Section(5, 10, 'rgba(0, 158, 122, 0.4)'),
+			Section(10, 15, 'rgba(54, 177, 56, 0.4)'),
+			Section(15, 20, 'rgba(111, 202, 56, 0.4)'),
+			Section(20, 25, 'rgba(248, 233, 45, 0.4)'),
+			Section(25, 30, 'rgba(253, 142, 42, 0.4)'),
+			Section(30, 40, 'rgba(236, 45, 45, 0.4)'),
+			Section(40, 100, 'rgba(245, 109, 205, 0.4)')
+		];
+	} else {
+		return [
+			Section(-200, -30, 'rgba(195, 92, 211, 0.4)'),
+			Section(-30, -25, 'rgba(139, 74, 197, 0.4)'),
+			Section(-25, -15, 'rgba(98, 65, 188, 0.4)'),
+			Section(-15, -5, 'rgba(62, 66, 185, 0.4)'),
+			Section(-5, 5, 'rgba(42, 84, 194, 0.4)'),
+			Section(5, 15, 'rgba(25, 112, 210, 0.4)'),
+			Section(15, 25, 'rgba(9, 150, 224, 0.4)'),
+			Section(25, 32, 'rgba(2, 170, 209, 0.4)'),
+			Section(32, 40, 'rgba(0, 162, 145, 0.4)'),
+			Section(40, 50, 'rgba(0, 158, 122, 0.4)'),
+			Section(50, 60, 'rgba(54, 177, 56, 0.4)'),
+			Section(60, 70, 'rgba(111, 202, 56, 0.4)'),
+			Section(70, 80, 'rgba(248, 233, 45, 0.4)'),
+			Section(80, 90, 'rgba(253, 142, 42, 0.4)'),
+			Section(90, 110, 'rgba(236, 45, 45, 0.4)'),
+			Section(110, 200, 'rgba(245, 109, 205, 0.4)')
+		];
+	}
+}
+
+/**
+ * //TODO move in Temp Gauge
+ * Converts a temperature trend value into a localised string, or +1, 0, -1 depending on the value of bTxt
+ * @param trend 
+ * @param units 
+ * @param bTxt 
+ * @param strings 
+ */
+function tempTrend(trend: number, units: TempUnit, strings?: Lang): Trendstate|string {
+	// Scale is over 3 hours, in Celsius
+	var val = trend * 3 * (units === "°C" ? 1 : (5 / 9));
+			
+	if (trend === -9999) 	return (strings ? '--' : TrendState.OFF);
+	else if (val > 5) 		return (strings ? strings.RisingVeryRapidly : TrendState.UP);
+	else if (val > 3)			return (strings ? strings.RisingQuickly : TrendState.UP);
+	else if (val > 1) 		return (strings ? strings.Rising : TrendState.UP);
+	else if (val > 0.5) 	return (strings ? strings.RisingSlowly : TrendState.UP);
+	else if (val >= -0.5) return (strings ? strings.Steady : TrendState.STEADY);
+	else if (val >= -1) 	return (strings ? strings.FallingSlowly : TrendState.DOWN);
+	else if (val >= -3) 	return (strings ? strings.Falling : TrendState.DOWN);
+	else if (val >= -5) 	return (strings ? strings.FallingQuickly : TrendState.DOWN);
+	else									return (strings ? strings.FallingVeryRapidly : TrendState.DOWN);
 }
 
 export default TempGauge;
