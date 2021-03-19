a0ca07e075dd3e4f9f18c48498ce740aba691006
Riccardo Rebottini <ricky@rebottini.it>
2021-03-19 12:57:11 +0100

    refactored UV gauge

--- src/gauges/uv.tsx
+++ src/gauges/uv.tsx
@@ -4,64 +4,43 @@ import { RadialBargraph, Section, GaugeType, gradientWrapper, rgbaColor } from "
 import styles from '../style/common.css';
 import { gaugeShadow, nextHighest } from './utils.js';
 import { RtData } from '../controller/types.js';
-import { CommonProps } from './types';
+import { CommonProps, RGBAColor } from './types';
+import { getCommonParams, SHADOW_COLOR, SHOW_GAUGE_SHADOW, SolarScaleDef, UV_LCD_DECIMALS } from './defaults';
 
 
 class UVGauge extends Component<CommonProps, State> {
 	static NAME = "UV_GAUGE";
 
 	canvasRef: React.RefObject<HTMLCanvasElement>;
-	gauge: any;
-	params: any;
+	gauge: RadialBargraph;
+
+	config: Config;
+
 	style: React.CSSProperties;
 
 	constructor(props: CommonProps) {
 		super(props);
 
 		this.canvasRef = React.createRef();
+
+		this.config = {
+			scaleDefMax: SolarScaleDef.Max_UV,
+			lcdDecimals: UV_LCD_DECIMALS,
+			useGradient: true,
+			showGaugeShadow: SHOW_GAUGE_SHADOW,
+			shadowColor: SHADOW_COLOR
+		}
 	
 		this.state = {
 			value:  0.0001,
-			maxValue:  this.props.controller.gaugeConfig.uvScaleDefMax,
+			maxValue:  this.config.scaleDefMax,
 			risk: '',
 
 			//popUpTxt: '',
-			//graph: ''
 		}
 
-		this.params = {
-			...this.props.controller.commonParams,
-			size: Math.ceil(this.props.size * this.props.controller.gaugeConfig.gaugeScaling),
-			gaugeType: GaugeType.TYPE3,
-			maxValue: this.state.maxValue,
-			titleString: this.props.controller.lang.uv_title,
-			niceScale: false,
-			section: [
-				Section(0, 2.9, '#289500'),
-				Section(2.9, 5.8, '#f7e400'),
-				Section(5.8, 7.8, '#f85900'),
-				Section(7.8, 10.9, '#d8001d'),
-				Section(10.9, 20, '#6b49c8')
-			],
-			useSectionColors: false,
-			valueGradient: new gradientWrapper(0, 16,
-				[0, 0.1, 0.19, 0.31, 0.45, 0.625, 1],
-				[
-					new rgbaColor(0, 200, 0, 1),
-					new rgbaColor(0, 200, 0, 1),
-					new rgbaColor(255, 255, 0, 1),
-					new rgbaColor(248, 89, 0, 1),
-					new rgbaColor(255, 0, 0, 1),
-					new rgbaColor(255, 0, 144, 1),
-					new rgbaColor(153, 140, 255, 1)
-				]
-			),
-			useValueGradient: true,
-			lcdDecimals:this.props.controller.gaugeConfig.uvLcdDecimals,
-		};
-
-		this.style = this.props.controller.gaugeConfig.showGaugeShadow
-			? gaugeShadow(this.params.size, this.props.controller.gaugeConfig.shadowColour)
+		this.style = this.config.showGaugeShadow
+			? gaugeShadow(this.props.size, this.config.shadowColor)
 			: {};
 
 		this.update = this.update.bind(this);
@@ -71,7 +50,37 @@ class UVGauge extends Component<CommonProps, State> {
 
 	componentDidMount() {
 		if(this.canvasRef.current) {
-			this.gauge = new RadialBargraph(this.canvasRef.current, this.params);
+			this.gauge = new RadialBargraph(this.canvasRef.current, {
+				...getCommonParams(),
+	
+				size: this.props.size,
+				gaugeType: GaugeType.TYPE3,
+				maxValue: this.state.maxValue,
+				titleString: this.props.controller.lang.uv_title,
+				niceScale: false,
+				section: [
+					Section(0, 2.9, '#289500'),
+					Section(2.9, 5.8, '#f7e400'),
+					Section(5.8, 7.8, '#f85900'),
+					Section(7.8, 10.9, '#d8001d'),
+					Section(10.9, 20, '#6b49c8')
+				],
+				useSectionColors: !this.config.useGradient,
+				valueGradient: new gradientWrapper(0, 16,
+					[0, 0.1, 0.19, 0.31, 0.45, 0.625, 1],
+					[
+						new rgbaColor(0, 200, 0, 1),
+						new rgbaColor(0, 200, 0, 1),
+						new rgbaColor(255, 255, 0, 1),
+						new rgbaColor(248, 89, 0, 1),
+						new rgbaColor(255, 0, 0, 1),
+						new rgbaColor(255, 0, 144, 1),
+						new rgbaColor(153, 140, 255, 1)
+					]
+				),
+				useValueGradient: this.config.useGradient,
+				lcdDecimals: this.config.lcdDecimals,
+			});
 			this.gauge.setValue(this.state.value);
 		}
 	}
@@ -81,6 +90,11 @@ class UVGauge extends Component<CommonProps, State> {
 
 		newState.value = UV;
 	
+		newState.maxValue = Math.max(
+			nextHighest(newState.value, 2),
+			this.config.scaleDefMax
+		);
+
 		let indx: number;
 		if (newState.value === 0) 			indx = 0;
 		else if (newState.value < 2.5) 	indx = 1;
@@ -88,11 +102,6 @@ class UVGauge extends Component<CommonProps, State> {
 		else if (newState.value < 7.5) 	indx = 3;
 		else if (newState.value < 10.5) indx = 4;
 		else 														indx = 5;
-
-		newState.maxValue = Math.max(
-			nextHighest(newState.value, 2),
-			this.props.controller.gaugeConfig.uvScaleDefMax
-		);
 
 		newState.risk = this.props.controller.lang.uv_levels[indx];
 
@@ -106,6 +115,7 @@ class UVGauge extends Component<CommonProps, State> {
 	componentDidUpdate(_prevProps: CommonProps, prevState: State) {
 		if(prevState.maxValue !== this.state.maxValue) {
 			this.gauge.setMaxValue(this.state.maxValue)
+			this.gauge.setValue(0);
 		}
 
 		this.gauge.setUnitString(this.state.risk);
@@ -117,8 +127,8 @@ class UVGauge extends Component<CommonProps, State> {
 			<div className={styles.gauge}>
 				<canvas 
 					ref={this.canvasRef}
-					width={this.params.size}
-					height={this.params.size}
+					width={this.props.size}
+					height={this.props.size}
 					style={this.style}
 				></canvas>
 			</div>
@@ -126,13 +136,21 @@ class UVGauge extends Component<CommonProps, State> {
 	}
 }
 
+
+interface Config {
+	scaleDefMax: number,
+	lcdDecimals: number,
+	useGradient: boolean,
+	showGaugeShadow: boolean,
+	shadowColor: RGBAColor
+}
+
 interface State {
 	value: number,
 	maxValue: number,
 	risk: string,
 
-	//popUpTxt: string,
-	//graph: string
+	//popUpTxt: string
 }
 
 export default UVGauge;
