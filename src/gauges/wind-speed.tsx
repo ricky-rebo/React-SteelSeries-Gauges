044f6eab20365e95825eb86d21deb252035ff78f
Riccardo Rebottini <ricky@rebottini.it>
2021-03-19 23:47:14 +0100

    refactored WindSpeed gauge

--- src/gauges/wind-speed.tsx
+++ src/gauges/wind-speed.tsx
@@ -2,55 +2,47 @@ import React, { Component } from 'react';
 // @ts-ignore
 import { Radial, Section } from "steelseries";
 import styles from '../style/common.css';
-import { gaugeShadow, nextHighest } from './utils.js';
-import { RtData, WindUnit } from '../controller/types.js';
-import { CommonProps } from './types';
+import { gaugeShadow, nextHighest } from './utils';
+import { RtData, WindUnit } from '../controller/types';
+import { CommonProps, RGBAColor/*, GaugeComponent*/ } from './types';
+import { getCommonParams, MIN_MAX_AREA_COLOR, SHADOW_COLOR, SHOW_GAUGE_SHADOW, WindScaleDef, WIND_AVG_AREA_COLOR } from './defaults';
 
 
-class WindSpeedGauge extends Component<CommonProps, State> {
+class WindSpeedGauge extends Component<CommonProps, State> { //GaugeComponent<CommonProps, State, Config> {
 	static NAME = "WINDSPEED_GAUGE";
 
 	canvasRef: React.RefObject<HTMLCanvasElement>;
-	gauge: any;
-	params: any;
+	gauge: Radial;
+
+	config: Config;
+
 	style: React.CSSProperties;
 
 	constructor(props: CommonProps) {
 		super(props);
 
 		this.canvasRef = React.createRef();
+
+		this.config = {
+			scaleDef: WindScaleDef,
+			windAvgAreaColor: WIND_AVG_AREA_COLOR,
+			minMaxAreaColor: MIN_MAX_AREA_COLOR,
+
+			showGaugeShadow: SHOW_GAUGE_SHADOW,
+			shadowColor: SHADOW_COLOR
+		}
 	
 		let { wind } = props.controller.getDisplayUnits();
-		let maxVal: number;
-		switch (wind) {
-			case "mph": maxVal = props.controller.gaugeConfig.windScaleDefMaxMph; break;
-			case "kts": maxVal = props.controller.gaugeConfig.windScaleDefMaxKts; break;
-			case "km/h": maxVal = props.controller.gaugeConfig.windScaleDefMaxKmh; break;
-			case "m/s": 	maxVal = props.controller.gaugeConfig.windScaleDefMaxMs; break;
-			default: maxVal = 0.0001;
-		}
 		this.state = {
 			value: 0.0001,
-			maxValue: maxVal,
+			maxValue: getMax(wind, 0.0001, this.config.scaleDef),
 			area: [],
 			maxGustToday: 0.0001,
 			displayUnit: wind
 		}
 
-		this.params = {
-			...props.controller.commonParams,
-			size: Math.ceil(props.size * props.controller.gaugeConfig.gaugeScaling),
-			maxValue: this.state.maxValue,
-			niceScale: false,
-			area: this.state.area,
-			maxMeasuredValueVisible: true,
-			titleString: props.controller.lang.wind_title,
-			unitString: this.state.displayUnit,
-			thresholdVisible: false,
-		};
-
-		this.style = this.props.controller.gaugeConfig.showGaugeShadow
-			? gaugeShadow(this.params.size, this.props.controller.gaugeConfig.shadowColour)
+		this.style = this.config.showGaugeShadow
+			? gaugeShadow(this.props.size, this.config.shadowColor)
 			: {};
 
 		this.update = this.update.bind(this);
@@ -60,12 +52,23 @@ class WindSpeedGauge extends Component<CommonProps, State> {
 
 	componentDidMount() {
 		if(this.canvasRef.current) {
-			this.gauge = new Radial(this.canvasRef.current, this.params);
+			this.gauge = new Radial(this.canvasRef.current, {
+				...getCommonParams(),
+	
+				size: this.props.size,
+				maxValue: this.state.maxValue,
+				niceScale: false,
+				area: this.state.area,
+				maxMeasuredValueVisible: true,
+				titleString: this.props.controller.lang.wind_title,
+				unitString: this.state.displayUnit,
+				thresholdVisible: false,
+			});
 			this.gauge.setValue(this.state.value);
 		}
 	}
 
-	async update({ wlatest, wgustTM, wspeed, wgust, windunit }: RtData) {
+	async update({ wlatest, wspeed, wgust, wgustTM, windunit }: RtData) {
 		let newState: any = {};
 
 		if(windunit !== this.state.displayUnit) {
@@ -79,30 +82,11 @@ class WindSpeedGauge extends Component<CommonProps, State> {
 		
 		//let maxAvgToday = extractDecimal(data.windTM);
 
-		switch (windunit) {
-			case "mph":
-			case "kts":
-				newState.maxValue = Math.max(
-					nextHighest(newState.maxGustToday, 10),
-					this.props.controller.gaugeConfig.windScaleDefMaxMph
-				);
-				break;
-			case "m/s":
-				newState.maxValue = Math.max(
-					nextHighest(newState.maxGustToday, 5),
-					this.props.controller.gaugeConfig.windScaleDefMaxMs
-				);
-				break;
-			default:
-				newState.maxValue = Math.max(
-					nextHighest(newState.maxGustToday, 20),
-					this.props.controller.gaugeConfig.windScaleDefMaxKmh
-				);
-		}
+		newState.maxValue = getMax(windunit, newState.maxGustToday, this.config.scaleDef);
 
 		newState.area=[
-			Section(0, average, this.props.controller.gaugeConfig.windAvgArea),
-			Section(average, gust, this.props.controller.gaugeConfig.minMaxArea)
+			Section(0, average, this.config.windAvgAreaColor),
+			Section(average, gust, this.config.minMaxAreaColor)
 		];
 
 		this.setState(newState);
@@ -128,8 +112,8 @@ class WindSpeedGauge extends Component<CommonProps, State> {
 			<div className={styles.gauge}>
 				<canvas 
 					ref={this.canvasRef}
-					width={this.params.size}
-					height={this.params.size}
+					width={this.props.size}
+					height={this.props.size}
 					style={this.style}
 				></canvas>
 				<div>
@@ -143,6 +127,16 @@ class WindSpeedGauge extends Component<CommonProps, State> {
 	}
 }
 
+
+interface Config {
+	scaleDef: typeof WindScaleDef,
+	windAvgAreaColor: RGBAColor,
+	minMaxAreaColor: RGBAColor,
+
+	showGaugeShadow: boolean,
+	shadowColor: RGBAColor
+}
+
 interface State {
 	value: number,
 	area: [],
@@ -150,8 +144,23 @@ interface State {
 	maxGustToday: number,
 	displayUnit: WindUnit
 
-	//popUpTxt: string,
-	//graph: string
+	//popUpTxt: string
+}
+
+
+function getMax(unit: WindUnit, value: number, scaleDef: typeof WindScaleDef) {
+	switch (unit) {
+		case "mph":
+			return Math.max(nextHighest(value, 10), scaleDef.Max_Mph);
+		case "kts":
+			return Math.max(nextHighest(value, 10), scaleDef.Max_Kts);
+		case "m/s":
+			return Math.max(nextHighest(value, 5), scaleDef.Max_Ms);
+		case "km/h":
+			return Math.max(nextHighest(value, 20), scaleDef.Max_Kmh);
+		default:
+			return 0.0001
+	}
 }
 
 export default WindSpeedGauge;
